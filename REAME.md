638563a014593dad6cd164b58a2334c0fb3a3922
jhonnshark <jhonnshark@gmail.com>
2024-01-01 14:47:57 -0500

    firstcommit

---
+++ REAME.md
@@ -0,0 +1,1 @@
+# panificador-gastos
---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import IconoNuevoGasto from './img/nuevo-gasto.svg';
+import Modal from './components/Modal';
+
+const App = () => {
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
+  const [gastos, setGastos] = useState([]);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 500);
+  };
+
+  const guardarGasto = (gasto) => {
+    console.log(gasto);
+  };
+
+  return (
+    <>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+      {isValidPresupuesto && (
+        <div className="nuevo-gasto">
+          <img
+            src={IconoNuevoGasto}
+            alt="Nuevo Gasto"
+            onClick={handleNuevoGasto}
+          />
+        </div>
+      )}
+      {modal && (
+        <Modal
+          setModal={setModal}
+          animarModal={animarModal}
+          setAnimarModal={setAnimarModal}
+          guardarGasto={guardarGasto}
+        />
+      )}
+    </>
+  );
+};
+
+export default App;
