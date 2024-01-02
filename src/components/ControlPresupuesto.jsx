fa98e73927e5eba78e7f91d7249028fafc93a535
jhonnshark <jhonnshark@gmail.com>
2024-01-02 00:14:55 -0500

    Calculado el Presupuesto Gastado

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
 import { formatearCantidad } from '../helpers';
 
-const ControlPresupuesto = ({ presupuesto }) => {
+const ControlPresupuesto = ({ gastos, presupuesto }) => {
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce(
+      (total, gasto) => total + gasto.cantidad,
+      0
+    );
+    setGastado(totalGastado);
+  }, [gastos]);
+
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
@@ -11,10 +23,10 @@ const ControlPresupuesto = ({ presupuesto }) => {
           <span>Presupuesto:</span> {formatearCantidad(presupuesto)}
         </p>
         <p>
-          <span>Disponible:</span> {formatearCantidad(0)}
+          <span>Disponible:</span> {formatearCantidad(disponible)}
         </p>
         <p>
-          <span>Gastado:</span> {formatearCantidad(0)}
+          <span>Gastado:</span> {formatearCantidad(gastado)}
         </p>
       </div>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import ControlPresupuesto from './ControlPresupuesto';
 import NuevoPresupuesto from './NuevoPresupuesto';
 
 const Header = ({
+  gastos,
   presupuesto,
   setPresupuesto,
   isValidPresupuesto,
@@ -11,7 +12,7 @@ const Header = ({
     <header>
       <h1>Planificador de Gastos</h1>
       {isValidPresupuesto ? (
-        <ControlPresupuesto presupuesto={presupuesto} />
+        <ControlPresupuesto gastos={gastos} presupuesto={presupuesto} />
       ) : (
         <NuevoPresupuesto
           presupuesto={presupuesto}
